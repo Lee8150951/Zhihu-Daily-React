a5a9506cb9ef4024c30c5aa88a9b6c5da05d6554
Lee8150951 <Liys9875@outlook.com>
2023-03-04 21:50:17 +0800

    2023-3-4-3

--- src/index.js
+++ src/index.js
@@ -4,11 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// REM配置
+import 'lib-flexible';
+
+// 样式
+import './assets/reset.min.css'
+import './index.less'
+
+// 处理最大宽度
+(function () {
+    const handleMax = () => {
+        let html = document.documentElement,
+            root = document.getElementById('root'),
+            deviceW = html.clientWidth;
+        root.style.maxWidth = '750px';
+        if (deviceW >= 750) {
+            html.style.fontSize = '75px';
+        }
+    };
+    handleMax();
+    window.addEventListener('resize', handleMax);
+})()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+      <div className="box">
+          Hello World
+      </div>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
