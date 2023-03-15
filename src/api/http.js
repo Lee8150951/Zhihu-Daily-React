8eadba7045d7f0610fc145381ed5129a8600d191
Lee8150951 <Liys9875@outlook.com>
2023-03-15 15:58:25 +0800

    2023-3-14

--- src/api/http.js
+++ src/api/http.js
@@ -1,108 +1,102 @@
 import _ from '../assets/utils';
 import qs from 'qs';
 import { Toast } from 'antd-mobile';
+import axios from 'axios';
 
-/* 核心方法 */
-const http = function http(config) {
-  // initial config & validate
-  if (!_.isPlainObject(config)) config = {};
-  config = Object.assign({
-    url: '',
-    method: 'GET',
-    credentials: 'include',
-    headers: null,
-    body: null,
-    params: null,
-    responseType: 'json',
-    signal: null,
-  }, config);
-  if (!config.url) throw new TypeError('url must be required');
-  if (!_.isPlainObject(config.headers)) config.headers = {};
-  if (config.params !== null && !_.isPlainObject(config.params)) config.params = null;
+// 配置默认请求路径
+let baseURL;
+if (process.env.NODE_ENV === 'development') {
+  // 开发、测试环境
+  baseURL = 'http://localhost:3000';
+} else if (process.env.NODE_ENV === 'production') {
+  // 正式环境
+  baseURL = 'http://localhost:3000';
+}
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 
-  let { url, method, credentials, headers, body, params, responseType, signal } = config;
-  if (params) {
-    url += `${url.includes('?') ? '&' : '?'}${qs.stringify(params)}`;
-  }
-  if (_.isPlainObject(body)) {
-    body = qs.stringify(body);
-    headers['Content-Type'] = 'application/x-www-form-urlencoded';
-  }
+// 创建axios实例
+const Axios = axios.create({
+  baseURL,
+  headers: {
+    post: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  },
+});
 
-  // 处理Token
+// 拦截器配置
+Axios.interceptors.request.use(config => {
   let token = _.storage.get('tk'),
-    safeList = ['/user_info', '/user_update', '/store', '/store_remove', '/store_list'];
+    safeList = ['/user_info', '/user_update', '/store', '/store_remove', '/store_list'],
+    url = config.url;
   if (token) {
     let reg = /\/api(\/[^?#]+)/,
       [, $1] = reg.exec(url) || [];
     let isSafe = safeList.some(item => {
       return $1 === item;
     });
-    if (isSafe) headers['authorization'] = token;
+    if (isSafe) {
+      config.headers.Authorization = token;
+    }
   }
+  return config;
+}, err => {
+  return Promise.reject(err);
+});
 
-  // send
-  method = method.toUpperCase();
-  config = {
-    method,
-    credentials,
-    headers,
-    cache: 'no-cache',
-    signal,
-  };
-  if (/^(POST|PUT|PATCH)$/i.test(method) && body) config.body = body;
-  return fetch(url, config)
-    .then(response => {
-      let { status, statusText } = response;
-      if (/^(2|3)\d{2}$/.test(status)) {
-        let result;
-        switch (responseType.toLowerCase()) {
-          case 'text':
-            result = response.text();
-            break;
-          case 'arraybuffer':
-            result = response.arrayBuffer();
-            break;
-          case 'blob':
-            result = response.blob();
-            break;
-          default:
-            result = response.json();
-        }
-        return result;
-      }
-      return Promise.reject({
-        code: -100,
-        status,
-        statusText,
-      });
-    })
-    .catch(reason => {
-      Toast.show({
-        icon: 'fail',
-        content: '网络繁忙,请稍后再试!',
-      });
-      return Promise.reject(reason);
-    });
+// 请求方法
+const request = function (url, config, method) {
+  return new Promise((resolve, reject) => {
+    if (method === 'get') {
+      let { params } = { ...config };
+      Axios.get(url, params)
+        .then(
+          res => {resolve(res.data)},
+          err => {
+            reject(err);
+            Toast.show({
+              icon: 'fail',
+              content: '网络繁忙,请稍后再试!',
+            });
+          }
+        )
+        .catch((err) => {
+          reject(err);
+          Toast.show({
+            icon: 'fail',
+            content: '网络繁忙,请稍后再试!',
+          });
+        });
+    } else if (method === 'post') {
+      Axios.post(url, config)
+        .then(
+          res => {resolve(res.data)},
+          err => {
+            reject(err);
+            Toast.show({
+              icon: 'fail',
+              content: '网络繁忙,请稍后再试!',
+            });
+          }
+        )
+        .catch((err) => {
+          reject(err);
+          Toast.show({
+            icon: 'fail',
+            content: '网络繁忙,请稍后再试!',
+          });
+        })
+    }
+  });
 };
 
-/* 快捷方法 */
-['GET', 'HEAD', 'DELETE', 'OPTIONS'].forEach(item => {
-  http[item.toLowerCase()] = function (url, config) {
-    if (!_.isPlainObject(config)) config = {};
-    config['url'] = url;
-    config['method'] = item;
-    return http(config);
-  };
-});
-['POST', 'PUT', 'PATCH'].forEach(item => {
-  http[item.toLowerCase()] = function (url, body, config) {
-    if (!_.isPlainObject(config)) config = {};
-    config['url'] = url;
-    config['method'] = item;
-    config['body'] = body;
-    return http(config);
-  };
-});
-
+const http = {
+  post: (url, params) => {
+    params = qs.stringify(params);
+    return request(url, params, 'post');
+  },
+  get: (url, params) => {
+    return request(url, { params }, 'get');
+  },
+};
 export default http;
